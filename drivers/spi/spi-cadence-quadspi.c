2b277d5f4016b8e4a4e271acf248ebbf72603eb6
Dinh Nguyen <dinguyen@kernel.org>
2021-11-08 14:08:54 -0600

    spi: cadence-quadspi: fix write completion support
    commit 98d948eb833104a094517401ed8be26ba3ce9935 upstream.
    
    Some versions of the Cadence QSPI controller does not have the write
    completion register implemented(CQSPI_REG_WR_COMPLETION_CTRL). On the
    Intel SoCFPGA platform the CQSPI_REG_WR_COMPLETION_CTRL register is
    not configured.
    
    Add a quirk to not write to the CQSPI_REG_WR_COMPLETION_CTRL register.
    
    Fixes: 9cb2ff111712 ("spi: cadence-quadspi: Disable Auto-HW polling)
    Signed-off-by: Dinh Nguyen <dinguyen@kernel.org>
    Reviewed-by: Pratyush Yadav <p.yadav@ti.com>
    Link: https://lore.kernel.org/r/20211108200854.3616121-1-dinguyen@kernel.org
    Signed-off-by: Mark Brown <broonie@kernel.org>
    [IA: backported for linux=5.15.y]
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/spi/spi-cadence-quadspi.c
+++ drivers/spi/spi-cadence-quadspi.c
@@ -37,6 +37,7 @@
 /* Quirks */
 #define CQSPI_NEEDS_WR_DELAY		BIT(0)
 #define CQSPI_DISABLE_DAC_MODE		BIT(1)
+#define CQSPI_NO_SUPPORT_WR_COMPLETION  BIT(3)
 
 /* Capabilities */
 #define CQSPI_SUPPORTS_OCTAL		BIT(0)
@@ -817,9 +818,11 @@ static int cqspi_write_setup(struct cqspi_flash_pdata *f_pdata,
 	 * polling on the controller's side. spinand and spi-nor will take
 	 * care of polling the status register.
 	 */
-	reg = readl(reg_base + CQSPI_REG_WR_COMPLETION_CTRL);
-	reg |= CQSPI_REG_WR_DISABLE_AUTO_POLL;
-	writel(reg, reg_base + CQSPI_REG_WR_COMPLETION_CTRL);
+	if (cqspi->wr_completion) {
+		reg = readl(reg_base + CQSPI_REG_WR_COMPLETION_CTRL);
+		reg |= CQSPI_REG_WR_DISABLE_AUTO_POLL;
+		writel(reg, reg_base + CQSPI_REG_WR_COMPLETION_CTRL);
+	}
 
 	reg = readl(reg_base + CQSPI_REG_SIZE);
 	reg &= ~CQSPI_REG_SIZE_ADDRESS_MASK;
@@ -1608,6 +1611,10 @@ static int cqspi_probe(struct platform_device *pdev)
 	cqspi->master_ref_clk_hz = clk_get_rate(cqspi->clks[CLK_QSPI_REF]);
 
 	master->max_speed_hz = cqspi->master_ref_clk_hz;
+
+	/* write completion is supported by default */
+        cqspi->wr_completion = true;
+
 	ddata  = of_device_get_match_data(dev);
 	if (ddata) {
 		if (ddata->quirks & CQSPI_NEEDS_WR_DELAY)
@@ -1619,6 +1626,8 @@ static int cqspi_probe(struct platform_device *pdev)
 
 		if (!(ddata->quirks & CQSPI_DISABLE_DAC_MODE))
 			cqspi->use_direct_mode = true;
+		if (ddata->quirks & CQSPI_NO_SUPPORT_WR_COMPLETION)
+                        cqspi->wr_completion = false;
 	}
 
 	ret = devm_request_irq(dev, irq, cqspi_irq_handler, 0,
@@ -1722,6 +1731,10 @@ static const struct cqspi_driver_platdata cdns_qspi = {
 	.quirks = CQSPI_DISABLE_DAC_MODE,
 };
 
+static const struct cqspi_driver_platdata socfpga_qspi = {
+       .quirks = CQSPI_NO_SUPPORT_WR_COMPLETION,
+};
+
 static const struct cqspi_driver_platdata k2g_qspi = {
 	.quirks = CQSPI_NEEDS_WR_DELAY,
 };
@@ -1752,6 +1765,10 @@ static const struct of_device_id cqspi_dt_ids[] = {
 		.compatible = "intel,lgm-qspi",
 		.data = &intel_lgm_qspi,
 	},
+        {
+                .compatible = "intel,socfpga-qspi",
+                .data = (void *)&socfpga_qspi,
+        },
 	{ /* end of table */ }
 };
 
